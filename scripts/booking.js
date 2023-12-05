af4e5d4ed2d868db84de71c33af7d1d0623efb81
Ethen Mitterhuber <ethenmitterhuber@gmail.com>
2023-12-05 16:14:49 -0700

    Finished the javascript part of the assignment

--- scripts/booking.js
+++ scripts/booking.js
@@ -3,52 +3,129 @@
 // Do any of these variables need to be initialized when the page is loaded? 
 // When do they need to be reset or updated?
 
-let costPerDay = 0
-let daysSelected = 0
+const HALF_DAY_RATE = 20;
+const FULL_DAY_RATE = 35;
+
+let costPerDay = FULL_DAY_RATE;
+let daysSelected = 0;
+
 
 // containers needed
-const COST_CONTAINER = document.getElementById("calculated-cost")
+const COST_CONTAINER = document.getElementById("calculated-cost");
 
 // week buttons
-const MONDAY_BUTTON = document.getElementById("moday")
-const TUESDAY_BUTTON = document.getElementById("tuesday")
-const WEDNESDAY_BUTTON = document.getElementById("wednesday")
-const THURSDAY_BUTTON = document.getElementById("thursday")
-const FRIDAY_BUTTON = document.getElementById("friday")
+const MONDAY_BUTTON = document.getElementById("monday");
+const TUESDAY_BUTTON = document.getElementById("tuesday");
+const WEDNESDAY_BUTTON = document.getElementById("wednesday");
+const THURSDAY_BUTTON = document.getElementById("thursday");
+const FRIDAY_BUTTON = document.getElementById("friday");
+
+//now create an array of these buttons to make looping through them easier
+const DAY_BUTTONS = [
+    MONDAY_BUTTON,
+    TUESDAY_BUTTON,
+    WEDNESDAY_BUTTON,
+    THURSDAY_BUTTON,
+    FRIDAY_BUTTON
+];
 
 // day buttons
-const FULL_DAY_BUTTON = document.getElementById("full")
-const HALF_DAY_BUTTON = document.getElementById("half")
+const FULL_DAY_BUTTON = document.getElementById("full");
+const HALF_DAY_BUTTON = document.getElementById("half");
+
+//clear button
+const CLEAR_BUTTON = document.getElementById("clear-button");
 
 /********* colour change days of week *********/
 // when the day buttons are clicked, we will apply the "clicked" class to that element, and update any other relevant variables. Then, we can recalculate the total cost.
 // added challenge: don't update the dayCounter if the same day is clicked more than once. hint: .classList.contains() might be helpful here!
 
+//build a function to set the days
+const addDay = (event) => {
+    let element = event.target;
+    let prevClicked = element.classList.contains("clicked");
 
+    //check if the element contains the 'clicked' class
+    if (prevClicked) {
+        daysSelected--;
+        element.classList.remove("clicked");
+    } else {
+        daysSelected++;
+        element.classList.add("clicked");
+    }
 
+    calculate();
+};
 
+//Add an eventlistener to the day buttons that calls the addDay function
+for (let i = 0; i < DAY_BUTTONS.length; i++) {
+    let currButton = DAY_BUTTONS[i];
+    currButton.addEventListener("click", addDay);
+}
 
 /********* clear days *********/
 // when the clear-button is clicked, the "clicked" class is removed from all days, any other relevant variables are reset, and the calculated cost is set to 0.
 
+const clearDays = () => {
 
+    //reset the daysSelected to 0, and clear the clicked class from the day buttons
+    daysSelected = 0;
 
+    for (let i = 0; i < DAY_BUTTONS.length; i++) {
+        let currButton = DAY_BUTTONS[i];
+        currButton.classList.remove("clicked");
+    }
+    
+    //recalculate, to ensure the rest of the page knows what is going on
+    calculate();
+};
 
+//add an event listener to the clear-days button that calls clearDays
+CLEAR_BUTTON.addEventListener("click", clearDays);
 
 
 /********* change rate *********/
 // when the half-day button is clicked, set the daily rate to $20, add the "clicked" class to the "half" element, remove it from the "full" element, and recalculate the total cost.
 
+const changeRateToHalf = () => {
 
+    //set the cost per day to the half day rate
+    costPerDay = HALF_DAY_RATE;
+
+    //set the half day to clicked, and the full day to not clicked
+    HALF_DAY_BUTTON.classList.add("clicked");
+    FULL_DAY_BUTTON.classList.remove("clicked");
+
+    //recalculate
+    calculate();
+};
 
 
 // when the full-day button is clicked, the daily rate is set back to $35, the clicked class is added to "full" and removed from "half", and the total cost is recalculated.
 
+const changeRateToFull = () => {
+
+    //set the cost per day to the half day rate
+    costPerDay = FULL_DAY_RATE;
+
+    //set the full day to clicked and the half day to not clicked
+    FULL_DAY_BUTTON.classList.add("clicked");
+    HALF_DAY_BUTTON.classList.remove("clicked");
+
+    //recalculate
+    calculate();
+};
 
 
-
+//add event listeners to the two rate buttons
+FULL_DAY_BUTTON.addEventListener("click", changeRateToFull);
+HALF_DAY_BUTTON.addEventListener("click", changeRateToHalf);
 
 /********* calculate *********/
 // when a calculation is needed, set the innerHTML of the calculated-cost element to the appropriate value
 
+const calculate = () => {
+    let cost = daysSelected * costPerDay;
 
+    COST_CONTAINER.innerHTML = cost;
+};
--- scripts/contact.js
+++ scripts/contact.js
@@ -2,3 +2,35 @@
 
 // hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
 
+//define some constants
+
+//this will allow the deletion of the child elements, or in other words, the form
+const MAIN = document.getElementsByTagName("main")[0]; //there will only ever be one main
+
+const sayThanks = () => {
+    //delete all of main's children
+
+    //get an iterable of main's child nodes
+    let mainChildren = MAIN.childNodes;
+
+    //remove each child
+    while (mainChildren.length > 0) {
+        mainChildren[0].remove();
+    }
+
+    //create a p tag with the needed styling
+    let newElement = document.createElement("p");
+    newElement.innerHTML = "Thank you for your message";
+    newElement.classList.add("large-text");
+
+    MAIN.appendChild(newElement);
+};
+
+//get the url, and if there is a ? attached to the end, then we know the button was clicked
+const CURR_URL = location.href;
+
+//the url will be checked once at the beginning of the page being loaded
+//if the path contains a ? then we know that the user has clicked the submit button.
+if (CURR_URL.endsWith("?")) {
+    sayThanks();
+}
